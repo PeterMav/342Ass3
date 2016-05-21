e76ce30bdaaf5f29631d8461b04e3822f5b33420
PeterMav <peter.mavo@gmail.com>
2016-05-21 21:12:06 +1000

    .

--- 342Ass3/342Ass3/SatelliteViewController.swift
+++ 342Ass3/342Ass3/SatelliteViewController.swift
@@ -17,82 +17,22 @@ class SatelliteViewController: UIViewController {
     let apiURL:String = "https://api.nasa.gov/planetary/earth/imagery"
     let lat:String = "-34.424984"
     let lon:String = "150.8931239"
-    // Pass an object around for map and date.
-    struct mapInfo {
-        var mapURL: String
-        var locationDate: String
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        performNASARequest("2015-09-19"){ result, dataRetrieved in
-            if result{
-                self.fetchImage(dataRetrieved!)
-            } else {
-                print("Nothing to retrieve")
-            }
-        }
+        performNASARequest()
     }
     
-    func fetchImage(location: mapInfo){
-        if let url = NSURL(string: location.mapURL){
-            let ns = NSURLSession.sharedSession()
-            ns.dataTaskWithURL(url, completionHandler: {(data, response, error) in
-                if error != nil {
-                    print("error with image url")
-                } else if let imageData = data, image = UIImage(data: imageData) {
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.mapImage.image = image
-                        self.dateLabel.text = location.locationDate
-                    })
-                }
-            }).resume()
-        }
+    func fetchImage(mapURL: String){
+        
     }
     
-    func performNASARequest(date: String, completion: (result: Bool, data:mapInfo?) -> ()){
-        let keys: Dictionary = [
-            "api_key" : api_key,
-            "lat" : lat,
-            "lon" : lon,
-            "date" : date,
-            "cloud_score": "True"
-        ]
-        
-        let urlComp = NSURLComponents(string: apiURL)!
-        
-        var query = [NSURLQueryItem]()
-        for (key, value) in keys {
-            query.append(NSURLQueryItem(name: key, value: value))
-        }
-        urlComp.queryItems = query
-        
-        let url = urlComp.URL!
-        
-        let request = NSMutableURLRequest(URL: url)
-        request.HTTPMethod = "GET"
-        
-        let ns = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        
-        ns.dataTaskWithRequest(request) { (data, response, error) -> Void in
-            if let response = response as? NSHTTPURLResponse where 200...299 ~= response.statusCode {
-                do{
-                    let JSON = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments )
-                    if let result = JSON as? NSDictionary{
-                        let mapURL = result["url"] as? String
-                        let locationDate = result["date"] as? String
-                        if mapURL != nil {
-                            completion(result: true, data: mapInfo(mapURL: mapURL!, locationDate: locationDate!))
-                        }else{
-                            print("Error getting image")
-                        }
-                        
-                    }
-                } catch {
-                    print("Timed out")
-                    completion(result: false, data: nil)
-                 }
-            }
-        }.resume()
+    func performNASARequest(){
+        let urlComp = NSURLComponents(string: apiURL)
+        let longitudeParam = NSURLQueryItem(name: "lon", value: String(self.lon))
+        let latitudeParam = NSURLQueryItem(name: "lat", value: String(self.lat))
+        let apiParam = NSURLQueryItem(name: "api_key", value: self.api_key)
+        urlComp!.queryItems = [longitudeParam, latitudeParam, apiParam]
     }
 }
