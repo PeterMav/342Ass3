46e711ec6d67ac5d3714bd558c523a210374fc83
PeterMav <peter.mavo@gmail.com>
2016-05-21 22:23:56 +1000

    .

--- 342Ass3/342Ass3/SatelliteViewController.swift
+++ 342Ass3/342Ass3/SatelliteViewController.swift
@@ -14,25 +14,65 @@ class SatelliteViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     let api_key:String = "kDL3uN6O7Dfe1pdladM4OYlIGcwe2J6e4rnUoC5F"
-    let apiURL:String = "https://api.nasa.gov/planetary/earth/imagery"
+    let baseURL:String = "https://api.nasa.gov/planetary/earth/imagery"
     let lat:String = "-34.424984"
     let lon:String = "150.8931239"
+    let date:String = "2000-09-19"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        performNASARequest()
+        performNASARequest(lat, lon: lon, date: date)
     }
     
-    func fetchImage(mapURL: String){
+    
+    
+    func performNASARequest(lat: String, lon: String, date: String){
+        
+        let urlComp = NSURLComponents(string: baseURL)
+        let urlLongitude = NSURLQueryItem(name: "lon", value: String(self.lon))
+        let urlLatitude = NSURLQueryItem(name: "lat", value: String(self.lat))
+        let api = NSURLQueryItem(name: "api_key", value: self.api_key)
+        let urlDate = NSURLQueryItem(name: "date", value: self.date)
+        urlComp!.queryItems = [urlLongitude, urlLatitude, api, urlDate]
+        
+//        print(lat)
+//        print(lon)
+//        print(date)
+//        print(urlLatitude)
+//        print(urlLongitude)
+//        print(urlDate)
+//        print(urlComp!)
+        let urlRequest = NSMutableURLRequest(URL: urlComp!.URL!)
+        let ss = NSURLSession.sharedSession()
+        
+        let task = ss.dataTaskWithRequest(urlRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                
+                do{
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options:.AllowFragments)
+                    
+                    let mapURL = json["url"] as! String
+                    let mapDate = json["date"] as! String
+                    self.fetchImage(mapURL, mapDate: mapDate)
+                    
+                }catch {
+                    fatalError("Error with Json: \(error)")
+                }
+            }
+        }
+        task.resume()
         
     }
     
-    func performNASARequest(){
-        let urlComp = NSURLComponents(string: apiURL)
-        let longitudeParam = NSURLQueryItem(name: "lon", value: String(self.lon))
-        let latitudeParam = NSURLQueryItem(name: "lat", value: String(self.lat))
-        let apiParam = NSURLQueryItem(name: "api_key", value: self.api_key)
-        urlComp!.queryItems = [longitudeParam, latitudeParam, apiParam]
+    func fetchImage(mapURL: String, mapDate: String){
+        print(mapURL)
+        print(mapDate)
+        
     }
 }
